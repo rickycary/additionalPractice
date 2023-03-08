da2a270fe27680e1bc5070bba144d70ff066fab1
Richard <ricky.cary1@gmail.com>
2023-03-07 18:57:28 -0600

    vue: Added vue101

---
+++ vue/setup/vuepractice/vue101/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>Hello World</h1>
+<Other  cheese="gouda"/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Other from "./components/Other-1.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Other
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/setup/vuepractice/vue101/src/components/Other-1.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+<h1>{{ cheese }}</h1>
+<h2>{{ count }}</h2>
+<button v-on:click="addOne">Add One</button>
+<ul>
+    <li v-for="num of numbers" v-bind:key="num">{{ num }}</li>
+</ul>
+<input type="text" v-model="input"/>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Other-1",
+    props: ["cheese"],
+    data: function(){
+
+        return{
+            count: 0,
+            numbers: [1, 2, 3, 4, 5],
+            input: ""
+        }
+    }, 
+    methods: {
+        addOne: function(){
+            alert(this.input)
+            this.count += 1
+        }
+    }
+}
+
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
